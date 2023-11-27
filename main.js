0c89acf687314d4f5bd05a61876d404b50ae50d4
Nicolas Minatti <nminatti@gmail.com>
2023-11-26 23:23:55 -0300

    First commit

---
+++ main.js
@@ -0,0 +1,6 @@
+import './style.css';
+// import javascriptLogo from './javascript.svg';
+// import viteLogo from '/vite.svg';
+// import { setupCounter } from './counter.js';
+
+import'./src/blackjack'; //busca por defecto el index.js (es opcional agregarlo a la importacion)
